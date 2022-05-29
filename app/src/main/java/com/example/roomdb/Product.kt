689b2812a07266d06bf52ba6c8c671614a3111a3
Daniel39177937 <39177937wasde@gmail.com>
2022-05-30 02:56:19 +0800

    V1

---
+++ app/src/main/java/com/example/roomdb/Product.kt
@@ -0,0 +1,27 @@
+package com.example.roomdb
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.w3c.dom.NameList
+
+@Entity(tableName = "products" )
+class Product {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="pid")
+    var id: Int=0
+    @ColumnInfo(name = "proName")
+    var pname: String=""
+    @ColumnInfo(name="qty")
+    var qty: Int=0
+    constructor(){}
+    constructor(id:Int,name:String,value:Int){
+        this.id=id
+        this.pname=name
+        this.qty=value
+    }
+    constructor(name:String,value:Int){
+        this.pname=name
+        this.qty=value
+    }
+}
